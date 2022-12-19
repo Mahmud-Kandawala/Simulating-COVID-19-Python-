7df715b9a483e2176e931450d61c85759a9206b9
Mahmud <mahmud.kandawala1@gmail.com>
2022-12-19 09:46:57 -0500

    Update virus.py

--- virus.py
+++ virus.py
@@ -19,10 +19,53 @@ COVID19_PARAMS = {
     "serial_interval": 7
 }
 
-class Virus(): 
+class Virus():
+    def __init__(self, params):
+        # create plot
+        self.fig = plt.figure()
+        self.axes = self.fig.add_subplot(111, projection="polar")
+        self.axes.grid(False)
+        self.axes.set_xticklabels([])
+        self.axes.set_yticklabels([])
+        self.axes.set_ylim(0, 1)
 
+        # create annotations
+        self.day_text = self.axes.annotate(
+            "Day 0", xy=[np.pi / 2, 1], ha="center", va="bottom")
+        self.infected_text = self.axes.annotate(
+            "Infected: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=RED)
+        self.deaths_text = self.axes.annotate(
+            "\nDeaths: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=BLACK)
+        self.recovered_text = self.axes.annotate(
+            "\n\nRecovered: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=GREEN)
 
+        # create member variables
+        self.day = 0
+        self.total_num_infected = 0
+        self.num_currently_infected = 0
+        self.num_recovered = 0
+        self.num_deaths = 0
+        self.r0 = params["r0"]
+        self.percent_mild = params["percent_mild"]
+        self.percent_severe = params["percent_severe"]
+        self.fatality_rate = params["fatality_rate"]
+        self.serial_interval = params["serial_interval"]
 
+        self.mild_fast = params["incubation"] + params["mild_recovery"][0]
+        self.mild_slow = params["incubation"] + params["mild_recovery"][1]
+        self.severe_fast = params["incubation"] + params["severe_recovery"][0]
+        self.severe_slow = params["incubation"] + params["severe_recovery"][1]
+        self.death_fast = params["incubation"] + params["severe_death"][0]
+        self.death_slow = params["incubation"] + params["severe_death"][1]
 
-    
-}
+        self.mild = {i: {"thetas": [], "rs": []} for i in range(self.mild_fast, 365)}
+        self.severe = {
+            "recovery": {i: {"thetas": [], "rs": []} for i in range(self.severe_fast, 365)},
+            "death": {i: {"thetas": [], "rs": []} for i in range(self.death_fast, 365)}
+        }
+
+        self.exposed_before = 0
+        self.exposed_after = 1
+
+        self.initial_population()
+
